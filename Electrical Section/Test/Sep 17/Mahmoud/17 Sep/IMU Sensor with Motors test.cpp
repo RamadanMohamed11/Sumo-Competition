e9a3d179d4bd49b63fc8a1ad1614a6d1155ccd6a
MAMFMAHMOUD <35840380+MAMFMAHMOUD@users.noreply.github.com>
2024-09-18 21:33:04 +0300

    Delete Electrical Section/Test/Sep 17/Mahmoud directory

--- Electrical Section/Test/Sep 17/Mahmoud/17 Sep/IMU Sensor with Motors test.cpp
+++
@@ -1,6 +0,0 @@
-
-/* --------------------------- Define Motors Pins --------------------------- */
-#define LEFT_MOTOR_P1 15
-#define LEFT_MOTOR_P2 2
-#define RIGHT_MOTOR_P1 18
-#define RIGHT_MOTOR_P2 19
